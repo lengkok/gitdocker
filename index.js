48db7d2c4e6d752c31ab673c352ea7ce3d2d82c6
lengkok <165160486+lengkok@users.noreply.github.com>
2024-05-20 18:47:16 +0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express ();
-app.get('/user', (req, res) => {
-    res.send({ "id": 11, "name": "Lengkok" });
-});
-app.listen(600, () => {
-    console.log('Express Server is Running at port no 600...');
-});
